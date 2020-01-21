bf78c3ecfd8b89d427fed84b0fa317a2e94a1556
chenun123 <chenun123@hotmail.com>
2020-01-22 01:41:59 +0800

    用户针对高版本npm,gulp 支持

---
+++ dev/leanote_gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var fs = require('fs')
+var path = require('path')
+var gulp = require('gulp')
+var gutil = require('gulp-util')
+var less = require('gulp-less');
+var cleanCSS = require('gulp-clean-css');
+
+var styleDir = '../public/themes';
+var styleDir2 = '../public/css';
+
+// 解析less
+gulp.task('less', done => {
+    gulp.src(styleDir + '/**/*.less')
+        .pipe(less())
+        .pipe(cleanCSS({compatibility: 'ie8', processImportFrom: ['!icon/iconfont.css', '!inhope-icon/style.css']}))
+        .pipe(gulp.dest(styleDir))
+        .pipe(gulp.dest(styleDir));
+
+    gulp.src(styleDir2 + '/**/*.less')
+        .pipe(less())
+        .pipe(cleanCSS({compatibility: 'ie8', processImportFrom: ['!icon/iconfont.css', '!inhope-icon/style.css']}))
+        .pipe(gulp.dest(styleDir2))
+        .pipe(gulp.dest(styleDir2));
+
+    gutil.log(gutil.colors.green('less ok'));
+	done();
+});
+
+// 开发服务
+gulp.task('dev', gulp.series('less', function() {
+    gulp.watch(styleDir + '/**/*.less', gulp.series('less'));
+    gulp.watch(styleDir2 + '/**/*.less', gulp.series('less'));
+}));
+
+gulp.task('default', gulp.series('dev'));
